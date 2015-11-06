c67b9507969ef10f8255c3a1921e06638cd51fcd
Hugo Brilhante <hugobrilhante@gmail.com>
2015-11-06 20:13:47 -0200

    step-6 Using mixins
     - change FavoriteList
     - change FavoriteDetail

--- favorite/views.py
+++ favorite/views.py
@@ -1,55 +1,35 @@
+from rest_framework import generics
+from rest_framework import mixins
+
 from favorite.models import Favorite
 from favorite.serializers import FavoriteSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
 
-class FavoriteList(APIView):
-    """
-    List all favorite, or create a new favorite.
-    """
-    def get(self, request, format=None):
-        favorite = Favorite.objects.all()
-        serializer = FavoriteSerializer(favorite, many=True)
-        return Response(serializer.data)
+class FavoriteList(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
 
-    def post(self, request, format=None):
-        serializer = FavoriteSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class FavoriteDetail(APIView):
-    """
-    Retrieve, update or delete a favorite instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Favorite.objects.get(pk=pk)
+class FavoriteDetail(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-        except Favorite.DoesNotExist:
-            raise Http404
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        favorite = self.get_object(pk)
-        serializer = FavoriteSerializer(favorite)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        favorite = self.get_object(pk)
-        serializer = FavoriteSerializer(favorite, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        favorite = self.get_object(pk)
-        favorite.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
