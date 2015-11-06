c42432131b4f39c1d56f3e18f2a9baeb571e235a
Hugo Brilhante <hugobrilhante@gmail.com>
2015-11-06 20:21:05 -0200

    step-7 Using generic class based views
     - change FavoriteList
     - change FavoriteDetail

--- favorite/views.py
+++ favorite/views.py
@@ -1,35 +1,14 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from favorite.models import Favorite
 from favorite.serializers import FavoriteSerializer
 
 
-class FavoriteList(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class FavoriteList(generics.ListCreateAPIView):
     queryset = Favorite.objects.all()
     serializer_class = FavoriteSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class FavoriteDetail(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class FavoriteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Favorite.objects.all()
     serializer_class = FavoriteSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
