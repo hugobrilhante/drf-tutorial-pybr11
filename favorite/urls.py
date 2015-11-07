c3104b14a9aaf54e53ae746295c422029eef4763
Hugo Brilhante <hugobrilhante@gmail.com>
2015-11-06 23:52:01 -0200

    step-10 ViewSets
    - refactoring to use ViewSets
    - binding ViewSets to URLs explicitly

--- favorite/urls.py
+++ favorite/urls.py
@@ -1,14 +1,31 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from favorite import views
+from favorite.views import FavoriteViewSet, UserViewSet, api_root
 
-urlpatterns = [
-    url(r'^$', views.api_root),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-    url(r'^favorites/$', views.FavoriteList.as_view(), name='favorite-list'),
-    url(r'^favorites/(?P<pk>[0-9]+)/$', views.FavoriteDetail.as_view(), name='favorite-detail'),
-]
+favorite_list = FavoriteViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+favorite_detail = FavoriteViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+
+urlpatterns = format_suffix_patterns([
+    url(r'^$', api_root),
+    url(r'^favorites/$', favorite_list, name='favorite-list'),
+    url(r'^favorites/(?P<pk>[0-9]+)/$', favorite_detail, name='favorite-detail'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
+])
--- favorite/views.py
+++ favorite/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -12,32 +12,32 @@ from favorite.serializers import FavoriteSerializer, UserSerializer
 
 @api_view(('GET',))
 def api_root(request, format=None):
+    """
+    API root
+    """
     return Response({
         'users': reverse('user-list', request=request, format=format),
         'favorites': reverse('favorite-list', request=request, format=format)
     })
 
 
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class FavoriteList(generics.ListCreateAPIView):
+class FavoriteViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
     queryset = Favorite.objects.all()
     serializer_class = FavoriteSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-
-class FavoriteDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Favorite.objects.all()
-    serializer_class = FavoriteSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
